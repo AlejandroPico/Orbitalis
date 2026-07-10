7b20d811f985849ba742f3ba0ac08438717c5bff
Alejandro Pico <115283749+AlejandroPico@users.noreply.github.com>
2026-07-10 22:02:33 +0200

    Start Orbitalis through local web server

--- INICIAR_ORBITALIS.bat
+++ INICIAR_ORBITALIS.bat
@@ -1,2 +1,7 @@
 @echo off
-start "" "%~dp0index.html"
+setlocal
+cd /d "%~dp0"
+start "Orbitalis Server" /min powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0servidor.ps1"
+timeout /t 2 /nobreak >nul
+start "" "http://127.0.0.1:8765/"
+endlocal
